83bcd7c605d11e50d5e0766ab66f542b0318c612
Didor Ada <didor.dada@gmail.com>
2021-10-11 14:44:53 +0200

    liste ingredients, refactorisation

--- Program.cs
+++ Program.cs
@@ -10,12 +10,14 @@ namespace projet_pizza
         string nom;
         float prix;
         bool vegetarienne;
+        List<string> ingredients;
 
-        public pizza(string nom, float prix, bool vegetarienne)
+        public pizza(string nom, float prix, bool vegetarienne, List<string> ingredients)
         {
             this.nom = nom;
             this.prix = prix;
             this.vegetarienne = vegetarienne;
+            this.ingredients = ingredients;
         }
 
         public void Afficher()
@@ -23,9 +25,11 @@ namespace projet_pizza
             
             string badgeVegetarienne = vegetarienne ? " (V)" : ""; // ternaire version 1 ligne :
 
-            string nomAfficher = FormatPremiereLettreMajuscules(nom); // substring retire une lettre au choix
+            string nomAfficher = FormatPremiereLettreMajuscules(nom);
 
             Console.WriteLine(nomAfficher + badgeVegetarienne + " - " + prix + "€");
+            Console.WriteLine(string.Join(", ", ingredients));
+            Console.WriteLine();
         }
 
         private static string FormatPremiereLettreMajuscules(string s) // static car elle ne dépend pas des données de notre pizza
@@ -56,12 +60,12 @@ namespace projet_pizza
             pizza1.afficher();*/
 
             var pizzas = new List<pizza>() {
-               new pizza("4 fromages", 11.5f, true),
-               new pizza("indienne", 10.5f, false),
-               new pizza("mexicaine", 13f, false),
-               new pizza("margherita", 8f, true),
-               new pizza("calzone", 12f, false),
-               new pizza("complète", 9.5f, false)
+               new pizza("4 fromages", 11.5f, true, new List<string> { "cantal", "mozzarella", "fromage de chèvre", "gruyère" }),
+               new pizza("indienne", 10.5f, false, new List<string> { "curry", "mozzarella", "poulet de chèvre", "poivron", "oignon", "coriandre" }),
+               new pizza("mexicaine", 13f, false, new List<string> { "boeuf", "mozzarella", "maïs", "tomates", "oignon", "coriandre" }),
+               new pizza("margherita", 8f, true, new List<string> { "sauce tomate", "mozzarella", "basilic" }),
+               new pizza("calzone", 12f, false, new List<string> { "tomate", "jambon", "persil", "oignon" }),
+               new pizza("complète", 9.5f, false, new List<string> { "jambon", "oeuf", "fromage" }),
             };
 
             foreach(var pizza in pizzas)
